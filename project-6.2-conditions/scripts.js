b55aad58a81baee2bfa7e50a08e75eb456ec8e0d
MaryNaati <102638242+MaryNaati@users.noreply.github.com>
2022-04-07 23:50:26 -0600

    Updated

---
+++ project-6.2-conditions/scripts.js
@@ -0,0 +1,18 @@
+/* write a function named assignGrade that*/
+/* takes 1 argument, a number score*/
+
+
+function assignGrade (score1){
+    let grade = ""
+if (score1 >= 90) {
+    grade = "A";
+}
+    return grade;
+}
+
+/*
+
+
+ let yourGrade= assignGrade(95);
+
+document.write("Your final grade is" + yourGrade);
