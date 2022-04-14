7ed951f150a72fd469ff6b64bb518aafb86c3a8c
MaryNaati <102638242+MaryNaati@users.noreply.github.com>
2022-04-13 19:41:01 -0600

    Re-Did project 5.1 to test myself

--- project-6.2-conditions/scripts.js
+++ project-6.2-conditions/scripts.js
@@ -2,17 +2,61 @@
 /* takes 1 argument, a number score*/
 
 
-function assignGrade (score1){
+function assignGrade(score){
     let grade = ""
-if (score1 >= 90) {
-    grade = "A";
+    if(score >= 90) {
+        grade = "A"
+    }else if(score <= 89 && score >= 80){
+        grade = "B"
+    }else if(score <= 79 && score >= 70) {
+        grade = "C"
+    } else if(score <= 69 && score >= 60) {
+        grade = "D"
+    }else(score <= 59) {
+        grade = "F"
+    }
+
+    return grade
 }
-    return grade;
-}
 
-/*
+/* let yourGrade= assignGrade(95);
+
+document.write("Your final grade is" + yourGrade);*/
+
+let score = 99
+
+let grade = assignGrade(score); 
+let text = "Your final grade is " + yourGrade + "!"
+console.log(text)
 
 
- let yourGrade= assignGrade(95);
+score = 88
 
-document.write("Your final grade is" + yourGrade);
+grade = assignGrade(score1);
+
+text = "Your final grade is " + yourGrade + "!"
+console.log(text)
+
+
+score = 77
+
+grade = assignGrade(score1);
+
+text = "Your final grade is " + yourGrade + "!"
+console.log(text)
+
+
+score = 66
+
+grade = assignGrade(score1);
+
+text = "Your final grade is " + yourGrade + "!"
+console.log(text);
+
+
+score = 55
+
+grade = assignGrade(score1);
+
+text = "Your final grade is " + yourGrade + "!"
+console.log(text)
