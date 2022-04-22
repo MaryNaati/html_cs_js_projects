744e5cfc2963049e7334facb2581833876d515c2
MaryNaati <102638242+MaryNaati@users.noreply.github.com>
2022-04-21 22:10:21 -0600

    8.2

---
+++ project-8.2-loops/script.js
@@ -0,0 +1,13 @@
+//Write a for loop that will iterate from 0 to 10. 
+//What am I multiplying by?
+var multiplyBy = 9;
+for (let i = 0; i <= 10; i++){
+    var result = multiplyBy * i;
+    
+//For each iteration of the for loop, it will multiply the number by 9
+    if (i * 9 == 0);{
+
+// and log the result (e.g. "2 * 9 = 18").
+    console.log( i + " * 9" + " = " + result)
+    }
+}
